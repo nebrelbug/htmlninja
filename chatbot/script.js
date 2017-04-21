5c3a60f74c64baf2f4c372ceee45ecc054325491
nebrelbug <nebrelbug@gmail.com>
2017-04-20 20:43:13 -0600

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -1,6 +1,18 @@
+var input = "not yet";
+var output = 0;
+function getResponse {
+    if (input === "not yet") {
+        output = "Say something!";
+        if (input === "hi") {
+            output = "Hola!";
+        }
+}
+
+
 $(document).ready(function() {
     $('#button').click(function() {
-        var toAdd = $('input[name=checkListItem]').val();
-        $('.list').append('<div class="item">' + toAdd + '</div>');
+        var input = $('input[name=checkListItem]').val();
+        getResponse();
+        $('.list').append(output);
     });
 });
