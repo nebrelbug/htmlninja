2075f4d9edfc1715ecaf45020c9ef7bd728ef5bc
nebrelbug <nebrelbug@gmail.com>
2017-04-20 19:54:09 -0600

    Update script.js

--- movement/script.js
+++ movement/script.js
@@ -1,22 +1,57 @@
 $(document).ready(function() {
+	var xpos = 0;
+	var ypos = 0;
+	var xv = 0;
+	var yv = 0;
+	function keyTester () {
+    if (keys[38]) { //this checks if up arrow is pressed
+        yv = yv - 1;
+    }
+    if (keys[40]) {
+        yv = yv + 1;
+    }
+    if (keys[37]) {
+        xv = xv - 1;
+    }
+    if (keys[39]) {
+        xv = xv + 1;
+    }
+};
+	function movement() {
+    keyTester();
+    xpos = xpos + xv; //setting the positions to the positions + movement
+    ypos = ypos + yv;
+    xv = xv * 0.9; //slowing it down
+    yv = yv * 0.9;
+    $('img').css('left', xpos+'px');
+    $('img').css('top', ypos+'px');	
+		
     $(document).keydown(function(key) {
+	    movement();
+};
         switch(parseInt(key.which,10)) {
+			var keyArray = [];
 			// Left arrow key pressed
 			case 37:
-				$('img').css('left', '-=10px');
+				keyArray[37] = true;
 				break;
 			// Up Arrow Pressed
 			case 38:
-				$('img').css('top', '-=10px');
+				keyArray[38] = true;
 				break;
 			// Right Arrow Pressed
 			case 39:
-				$('img').css('left', '+=10px');
+				keyArray[39] = true;
 				break;
 			// Down Arrow Pressed
 			case 40:
-				$('img').css('top', '+=10px');
+				keyArray[40] = true;
 				break;
 		}
+	    
+
+
+
+
 	});
 });
