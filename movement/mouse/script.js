e512e7ea7cde4a67d4ae5160ca18eedff2ff654c
nebrelbug <nebrelbug@gmail.com>
2017-05-01 17:19:40 -0600

    Update script.js

--- movement/mouse/script.js
+++ movement/mouse/script.js
@@ -1,23 +1,24 @@
-var keys = [];
+var directions = [];
 var playing = 1;
 var xv = 0;
-	var yv = 0;
-	function keyAction () {
-    if (keys[38]) { //this checks if up arrow is pressed
+var yv = 0;
+
+	function mouseAction () {
+    if (directions[2]) { //this checks if up arrow is pressed
         yv = yv - 1;
     }
-    if (keys[40]) {
+    if (directions[4]) {
         yv = yv + 1;
     }
-    if (keys[37]) {
+    if (directions[1]) {
         xv = xv - 1;
     }
-    if (keys[39]) {
+    if (directions[3]) {
         xv = xv + 1;
     }
 }
 	function movement() {
-    keyAction();
+    mouseAction();
     $('#character').css('left', '+=' + xv + 'px');
     $('#character').css('top', '+=' + yv + 'px');	
     xv = xv * 0.9; //slowing it down
@@ -25,16 +26,27 @@ var xv = 0;
 	}
 
 $(document).ready(function() {
-
-
-$(document).keydown(function (e) {
-    keys[e.which] = true;
-	keyAction();
-	movement();
-});
-
-$(document).keyup(function (e) {
-    delete keys[e.which];
-    keyAction();
+$( "body" ).mouseover(function() {
+  if(event.pageX<xv) {
+	  directions[1]=true;
+  } else {
+	  directions[1]=false;
+	 }
+	if(event.pageX>xv) {
+	  directions[3]=true;
+  } else {
+	  directions[3]=false;
+	 }
+	if(event.pageY>yv) {
+	  directions[2]=true;
+  } else {
+	  directions[2]=false;
+	 }
+	if(event.pageY<yv) {
+	  directions[4]=true;
+  } else {
+	  directions[4]=false;
+	 }
+movement();
 });
 });
