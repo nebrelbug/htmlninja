3e4937321a8aae7edadcd5c2e2a16cd0229c5bd0
nebrelbug <nebrelbug@gmail.com>
2017-05-02 16:33:13 -0600

    Update script.js

--- movement/mouse/script.js
+++ movement/mouse/script.js
@@ -19,28 +19,29 @@ var yv = 0;
 }
 	function movement() {
     mouseAction();
-    $('#character').css('left', '+=' + xv + 'px');
-    $('#character').css('top', '+=' + yv + 'px');	
+    $('#character').css('left', '+=' + (xv+200) + 'px');
+    $('#character').css('top', '+=' + (yv+200) + 'px');	
     xv = xv * 0.9; //slowing it down
     yv = yv * 0.9;
 	}
 
 $(document).ready(function() {
 $( document ).on( "mousemove", function( event ) {
-  if(event.pageX<xv) {
+  if(event.pageX<xv+200) {
 	  directions[1]=true;
 	  directions[3]=false;
-  } if(event.pageX>xv) {
+  } if(event.pageX>xv+200) {
 	  directions[3]=true;
 	  directions[1]=false;
-  }if(event.pageY>yv) {
-	  directions[2]=true;
-	  directions[4]=false;
-  }if(event.pageY<yv) {
+  }if(event.pageY>yv+200) {
 	  directions[4]=true;
 	  directions[2]=false;
+  }if(event.pageY<yv+200) {
+	  directions[2]=true;
+	  directions[4]=false;
   }
 movement();
 	$('#directions').html(directions);
+	$('#coordinates').html(event.pageX + ',' event.pageY);
 });
 });
