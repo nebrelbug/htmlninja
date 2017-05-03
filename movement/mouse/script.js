75ae7d16509c06816a447a18c26c9d87227ec4ad
nebrelbug <nebrelbug@gmail.com>
2017-05-02 19:05:17 -0600

    Update script.js

--- movement/mouse/script.js
+++ movement/mouse/script.js
@@ -21,27 +21,30 @@ var yv = 0;
     mouseAction();
     /*$('#character').css('left', '+=' + xv + 'px');
     $('#character').css('top', '+=' + yv + 'px');*/
-    $( "#character" ).offset({ top: +=yv, left: +=xv });
+    xCoordinate+=xv;
+    yCoordinate+=yv;
+    $( "#character" ).offset({ top: xCoordinate, left: yCoordinate });
     xv = xv * 0.9; //slowing it down
     yv = yv * 0.9;
 	}
 
 $(document).ready(function() {
-$( document ).on( "mouseover", function( event ) {
+	$( "#character" ).offset({ top: 200, left: 200 });
 	var coordinate = $("#character").offset();
 	var xCoordinate = coordinate.left;
 	var yCoordinate = coordinate.top;
-	
-  if(event.pageX<xCoordinate) {
+$( document ).on( "mouseover", function( event ) {
+	coordinate = $("#character").offset();	
+  if(event.pageX<coordinate.left) {
 	  directions[1]=true;
 	  directions[3]=false;
-  } if(event.pageX>xCoordinate) {
+  } if(event.pageX>coordinate.left) {
 	  directions[3]=true;
 	  directions[1]=false;
-  }if(event.pageY>yCoordinate) {
+  }if(event.pageY>coordinate.top) {
 	  directions[4]=true;
 	  directions[2]=false;
-  }if(event.pageY<yCoordinate) {
+  }if(event.pageY<coordinate.top) {
 	  directions[2]=true;
 	  directions[4]=false;
   }
