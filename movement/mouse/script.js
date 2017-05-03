bd378a570dc0fa0f537b13a3c3ad95656cf3c38d
nebrelbug <nebrelbug@gmail.com>
2017-05-02 19:01:12 -0600

    Update script.js

--- movement/mouse/script.js
+++ movement/mouse/script.js
@@ -19,24 +19,29 @@ var yv = 0;
 }
 	function movement() {
     mouseAction();
-    $('#character').css('left', '+=' + xv + 'px');
-    $('#character').css('top', '+=' + yv + 'px');	
+    /*$('#character').css('left', '+=' + xv + 'px');
+    $('#character').css('top', '+=' + yv + 'px');*/
+    $( "#character" ).offset({ top: +=yv, left: +=xv });
     xv = xv * 0.9; //slowing it down
     yv = yv * 0.9;
 	}
 
 $(document).ready(function() {
-$( document ).on( "mousemove", function( event ) {
-  if(event.pageX<xv+200) {
+$( document ).on( "mouseover", function( event ) {
+	var coordinate = $("#character").offset();
+	var xCoordinate = coordinate.left;
+	var yCoordinate = coordinate.top;
+	
+  if(event.pageX<xCoordinate) {
 	  directions[1]=true;
 	  directions[3]=false;
-  } if(event.pageX>xv+200) {
+  } if(event.pageX>xCoordinate) {
 	  directions[3]=true;
 	  directions[1]=false;
-  }if(event.pageY>yv+200) {
+  }if(event.pageY>yCoordinate) {
 	  directions[4]=true;
 	  directions[2]=false;
-  }if(event.pageY<yv+200) {
+  }if(event.pageY<yCoordinate) {
 	  directions[2]=true;
 	  directions[4]=false;
   }
