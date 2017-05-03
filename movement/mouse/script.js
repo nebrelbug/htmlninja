4140f65ee1309bc1eafd75a3fd895cb1916e253d
nebrelbug <nebrelbug@gmail.com>
2017-05-02 19:51:46 -0600

    Update script.js

--- movement/mouse/script.js
+++ movement/mouse/script.js
@@ -9,16 +9,16 @@ var yv = 0;
 
 	function mouseAction () {
     if (directions[2]) { //this checks if up arrow is pressed
-        yv = yv - 1;
+        yv = yv - 0.4;
     }
     if (directions[4]) {
-        yv = yv + 1;
+        yv = yv + 0.4;
     }
     if (directions[1]) {
-        xv = xv - 1;
+        xv = xv - 0.4;
     }
     if (directions[3]) {
-        xv = xv + 1;
+        xv = xv + 0.4;
     }
 }
 	function movement() {
@@ -42,11 +42,11 @@ $( document ).on( "mousemove", function( event ) {
 	  directions[3]=true;
 	  directions[1]=false;
   }if(event.pageY>coordinate.top) {
-	  directions[4]=true;
-	  directions[2]=false;
-  }if(event.pageY<coordinate.top) {
 	  directions[2]=true;
 	  directions[4]=false;
+  }if(event.pageY<coordinate.top) {
+	  directions[4]=true;
+	  directions[2]=false;
   }
 movement();
 	$('#directions').html(directions);
