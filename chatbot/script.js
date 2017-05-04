0b6ff7bed32c47e1b051a410b3d2786ee499e493
nebrelbug <nebrelbug@gmail.com>
2017-05-04 17:54:26 -0600

    Update script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -3,15 +3,18 @@ var output = 0;
 function getResponse() {
     if (input === "not yet") {
         output = "Say something!";
-    }if (input === "hi") {
+    }if (input === "Hi") {
             output = "Hola!";
-        }
+    }if (input === "How are you?") {
+        output = "Good!";
+    }
         //output = $("<p>" + response + "</p>");
 }
 
 
 $(document).ready(function() {
     $('#button').click(function() {
+        getResponse();
         input = $('input[name=checkListItem]').val();
         $('.list').append('<div class="item">' + output + '</div>');
     });
